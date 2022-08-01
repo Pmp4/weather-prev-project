d9b702d9eff52e5e16f7a17b30ce849ae7617820
P__.mp4 <postlist.ed@gmail.com>
2022-08-01 23:22:59 +0900

    2022/08/01 23:22 react 설치/초기설정

--- src/main/java/com/pmp4/weatherprevproject/test/controller/TestController.java
+++ src/main/java/com/pmp4/weatherprevproject/test/controller/TestController.java
@@ -3,12 +3,15 @@ package com.pmp4.weatherprevproject.test.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class TestController {
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
@@ -18,6 +21,7 @@ public class TestController {
 
         Map<String, Object> res = new HashMap<>();
         res.put("SUCCESS", true);
+        res.put("SUCCESS_TEXT", "Hello SpringBoot/React");
 
         return res;
     }
