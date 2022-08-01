e9d0366f0ae6d27a65034a634d2f87039749df45
P__.mp4 <postlist.ed@gmail.com>
2022-08-01 21:49:34 +0900

    first commit

---
+++ src/main/java/com/pmp4/weatherprevproject/WeatherPrevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pmp4.weatherprevproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherPrevProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherPrevProjectApplication.class, args);
+    }
+
+}
